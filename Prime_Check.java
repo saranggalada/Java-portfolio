424e76f2c09fa3a37750ec870f49a320803a60f7
sarang123 <32020201+sarang123@users.noreply.github.com>
2022-02-10 16:54:22 +0530

    Create Prime_Check.java

---
+++ Prime_Check.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = input.nextInt(); //receives input
+        int x = 0;
+        for (int i = 2; i<n; i++) {
+            if(n%i==0) { //checks divisibility
+                x++;
+            }
+        }
+        
+        //decides output
+        if(x==0) { System.out.println(n+" is Prime");}
+        else { System.out.println(n+" is not Prime");}
+    }
+}
