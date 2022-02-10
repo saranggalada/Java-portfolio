196bafd2d415af0a5173f24426b01fa173b69e9c
sarang123 <32020201+sarang123@users.noreply.github.com>
2022-02-10 17:02:03 +0530

    Create Multiplication_Tables.java

---
+++ Multiplication_Tables.java
@@ -0,0 +1,17 @@
+//Prints multiplication tables of a number
+
+import java.util.Scanner;
+
+public class MultiplicationTables {
+    
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = inp.nextInt();
+        System.out.print("How many entries of the multiplication table of "+(n)+" do you want?: ");
+        int m = inp.nextInt();
+        for(int i=1;i<=m;i++) {
+            System.out.println(n+" x "+i+" = "+(i*n));
+        }
+    }
+}
