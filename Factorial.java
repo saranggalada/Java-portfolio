43931abaa365803bab7f4b2c0da8e157a855ebf9
sarang123 <32020201+sarang123@users.noreply.github.com>
2022-02-10 16:46:04 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Factorial {
+    
+    public static long fact(long n){
+        long f = 1;
+        while(n>0){
+            f*=n; n--;
+        }
+        return f;
+    }
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        long num = input.nextLong();
+        System.out.println(fact(num));
+    }
+}
